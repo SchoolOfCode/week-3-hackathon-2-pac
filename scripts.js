aa1137c8d0ace8efa3ba6368b6a9b7d26acffae9
Chris Lawlor <chris.d.lawlor@gmail.com>
2024-12-12 18:28:17 +0000

    added button fun ctionality to play a sound when clicked

--- scripts.js
+++ scripts.js
@@ -26,5 +26,25 @@ document.addEventListener("DOMContentLoaded", randoDog)
 const buttonNewDoggo = document.getElementById("newDoggo");
 buttonNewDoggo.addEventListener("click", randoDog);
 
-const hoverButtonNewDoggo = document.getElementById("newDoggo");
-buttonNewDoggo.addEventListener("hover", hoverButton);
+// const hoverButtonNewDoggo = document.getElementById("newDoggo");
+// buttonNewDoggo.addEventListener("hover", hoverButton);
+
+
+function clickBark() {
+    let barkingArray = [
+        "Bark1.mp3",
+        "Bark2.mp3",
+        "Bark3.mp3",
+        "Bark4.mp3",
+        "Bark5.mp3"
+    ];
+    
+    let randomNumber = Math.floor(Math.random() * (barkingArray.length));
+    let doggoSound = barkingArray[randomNumber];
+    console.log(doggoSound);
+    let audio = new Audio(doggoSound);
+    audio.play();
+};
+
+const buttonBark = document.getElementById("newDoggo");
+buttonBark.addEventListener("click", clickBark);
