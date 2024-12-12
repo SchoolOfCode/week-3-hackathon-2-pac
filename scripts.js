1033d3061395712317065e13dd597837d249792d
Adam Hemmings <38017169+Makenshii@users.noreply.github.com>
2024-12-12 16:20:21 +0000

    updated css

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 // fetch an image from Dog ceo
 
 async function randoDog() {
+    try {
     // fecth and return imageURL data
     const response = await fetch("https://dog.ceo/api/breeds/image/random");
     const data = await response.json();
@@ -11,6 +12,9 @@ async function randoDog() {
     // target the img to update the src
     const imageElement = document.getElementById("doggo");
     imageElement.src = imageURL;
-}; 
+    } catch (error) {
+        console.error(`Error fetching data:`, error);
+    };
+};
 
 document.addEventListener("DOMContentLoaded", randoDog)
